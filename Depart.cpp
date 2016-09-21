87c12d0803f1860fed4113f2ad2cb60a26cf0856
clifton.wilson <clifton.wilson@SEA-8D0AB9>
2016-09-21 09:32:03 -0700

    i push pong

--- Depart.cpp
+++ Depart.cpp
@@ -22,7 +22,8 @@ void Depart::draw()
 	sprintf_s(buffer, "Press 'Q' or Wait for Depart Time: %f", timer);
 	sfw::drawString(font, buffer, 100, 100, 16, 16);
 	sfw::drawLine(100, 80, 100 + 500 * (timer / 3.f), 80);
-	sfw::drawString(font, "You Scared?", 125, 425, 25, 25, 0, 0, WHITE);
+	sfw::drawString(font, "Until Next Time", 125, 425, 25, 25, 0, 0, WHITE);
+	sfw::drawString(font, "Muahahahahahahaha", 125, 350, 25, 25, 0, 0, WHITE);
 	sfw::setBackgroundColor(RED);
 }
 
